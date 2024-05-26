6cac7d47e86e38d0d94f5a843b4c7228e07821d5
Ernestolop <ernestodaniellopez504@gmail.com>
2024-05-26 15:15:03 -0400

    chore: add spring framework base

---
+++ backend/src/main/java/com/elopez/scrum/platform/ScrumPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.elopez.scrum.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/elopez/scrum/platform/ScrumPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elopez.scrum.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
